41d81883430481d94ea85c5bf038403c17ea4347
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2023-09-29 23:43:24 +0530

    all the problems practice on 29 september 2023

---
+++ LEETCODE/src/Date_09_29_2023/ValidPalindrome.java
@@ -0,0 +1,21 @@
+package Date_09_29_2023;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s) {
+        String cleanedString=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        int left=0;
+        int right=cleanedString.length()-1;
+        while(left<right){
+            if(cleanedString.charAt(left)!=cleanedString.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
