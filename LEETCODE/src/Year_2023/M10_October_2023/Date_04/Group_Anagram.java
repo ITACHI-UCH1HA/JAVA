07c25e35cd0746e395b3ac487f9f25b0317a9b63
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2023-10-05 23:08:02 +0530

    all the problems practiced on 5th october

---
+++ LEETCODE/src/Year_2023/M10_October_2023/Date_04/Group_Anagram.java
@@ -0,0 +1,16 @@
+package Year_2023.M10_October_2023.Date_04;
+import java.util.*;
+public class Group_Anagram {
+    public static void main(String[] args) {
+        String[] strs={"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result=new ArrayList<>();
+        HashMap<Character,Character> map=new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            
+        }
+        return result;
+    }
+}
--- LEETCODE/src/Year_2023/M10_October_2023/Date_04/Isomorphic_Strings.java
+++ LEETCODE/src/Year_2023/M10_October_2023/Date_04/Isomorphic_Strings.java
@@ -5,22 +5,42 @@ import java.util.HashMap;
 public class Isomorphic_Strings {
     public static void main(String[] args) {
         String s="egg";
-        String t="agg";
-        System.out.println(isIsomorphic_2(s,t));
+        String t="add";
+        System.out.println(isIsomorphic(s,t));
     }
     public static boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character,Integer> sCount=new HashMap<>();
-        HashMap<Character,Character> stConnect=new HashMap<>();
-        for (char c:s.toCharArray()) {
-            sCount.put(c,sCount.getOrDefault(c,0)+1);
+
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Character> stConnect = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            // Check if there's a mapping for sChar in stConnect
+            if (stConnect.containsKey(sChar)) {
+                if (stConnect.get(sChar) != tChar) {
+                    return false; // Mismatch found
+                }
+            } else {
+                stConnect.put(sChar, tChar); // Create a mapping from sChar to tChar
+            }
+
+            // Check if sChar occurs the same number of times in both s and t
+            sCount.put(sChar, sCount.getOrDefault(sChar, 0) + 1);
         }
-        for (char c:sCount.keySet()) {
-            stConnect.put(c,c);
+
+        // Ensure that no character in s maps to the same character in t more than once
+        for (char c : sCount.keySet()) {
+            if (sCount.get(c) > 1) {
+                return false; // Mismatch found
+            }
         }
-        return true;
+
+        return true; // No mismatches found
     }
     private static boolean isIsomorphic_2(String s, String t) {
         if(s.length()!=t.length()){
