5749f624d3ce50bbb54cbfbd9a4ab4066575432c
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2023-10-11 22:48:42 +0530

    all the problems solved on 11 october 2023

--- LEETCODE/src/Year_2023/M10_October_2023/Date_10/Main.java
+++ LEETCODE/src/Year_2023/M10_October_2023/Date_10/Main.java
@@ -1,7 +1,12 @@
 package Year_2023.M10_October_2023.Date_10;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Heap<Integer> heap=new Heap<>();
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+
     }
 }
